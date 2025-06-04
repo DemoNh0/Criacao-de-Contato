8831449fb7bbf3f3719d94484d1b251616d9b494
aluno <etecae023@hotmail.com>
2025-06-04 19:29:44 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'CadastroUser',
+  webDir: 'www'
+};
+
+export default config;
