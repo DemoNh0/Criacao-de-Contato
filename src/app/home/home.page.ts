2ac09ab19f89c7cfac7c4503799352ea54e1f120
aluno <etecae023@hotmail.com>
2025-06-04 21:00:11 -0300

    Primeiro cimmit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,11 @@
+import { ContatosService } from '../services/contatos.service';
 import { Component } from '@angular/core';
+
+interface Contato {
+  nome: string;
+  email: string;
+  telefone: string;
+}
 
 @Component({
   selector: 'app-home',
@@ -6,8 +13,46 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
   standalone: false,
 })
+
 export class HomePage {
 
-  constructor() {}
+  contatos: Contato[] = [];
+  contato: Contato = { nome: '', email: '', telefone: '' };
 
+  indiceEdicao: number = -1;
+
+  constructor(private ContatosService: ContatosService) {}
+
+  ngOnInit() {
+    this.carregarContatos();
+  }
+
+  async carregarContatos() {
+    this.contatos = await this.ContatosService.obterContatos();
+  }
+
+  async salvarContato() {
+    if (this.indiceEdicao >= 0) {
+      await this.ContatosService.atualizarContato(this.indiceEdicao, this.contato);
+    } else {
+      await this.ContatosService.adicionarContato(this.contato);
+    }
+    this.limparFormulario();
+    this.carregarContatos();
+  }
+
+  editarContato(indice: number) {
+    this.contato = { ...this.contatos[indice] };
+    this.indiceEdicao = indice;
+  }
+
+  async excluirContato(indice: number) {
+    await this.ContatosService.excluirContato(indice);
+    this.carregarContatos();
+  }
+
+  limparFormulario() {
+    this.contato = { nome: '', email: '', telefone: '' };
+    this.indiceEdicao = -1;
+  }
 }
